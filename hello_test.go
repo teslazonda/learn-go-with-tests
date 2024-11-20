4887552b228258ae5837c6d347ad9d45fd1b2589
Quill Cronwall <quilljazz@gmail.com>
2024-11-20 17:41:15 +0900

    Add tests for Spanish and French inputs

--- hello_test.go
+++ hello_test.go
@@ -5,17 +5,29 @@ import "testing"
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
 
-		got := Hello("teslazonda")
+		got := Hello("teslazonda", "english")
 		want := "Hello, teslazonda"
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("empty sting defaults to 'world'", func(t *testing.T) {
 
-		got := Hello("")
+		got := Hello("", "english")
 		want := "Hello, World"
 		assertCorrectMessage(t, got, want)
 	})
+	t.Run("in Spanish", func(t *testing.T) {
 
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+
+		got := Hello("Pierre", "French")
+		want := "Bonjour, Pierre"
+		assertCorrectMessage(t, got, want)
+	})
 }
 
 func assertCorrectMessage(t testing.TB, got string, want string) {
