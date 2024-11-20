ca38428897b20b68ad58d9d8ba4fbb8288f7f9e0
Quill Cronwall <quilljazz@gmail.com>
2024-11-20 21:52:36 +0900

    Final refactor

--- hello.go
+++ hello.go
@@ -1,26 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-const spanish = "Spanish"
-const french = "French"
-const englishHelloPrefix = "Hello, "
-const spanishHelloPrefix = "Hola, "
-const frenchHelloPrefix = "Bonjour, "
+const (
+	spanish            = "Spanish"
+	french             = "French"
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
 
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	prefix := englishHelloPrefix
+	return greetingPrefix(language) + name
+
+}
+func greetingPrefix(language string) (prefix string) {
+
 	switch language {
 	case spanish:
 		prefix = spanishHelloPrefix
 	case french:
 		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-
-	return prefix + name
+	return
 }
 func main() {
 	fmt.Println(Hello("teslazonda", "english"))
