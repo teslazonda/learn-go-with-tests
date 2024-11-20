2e4f49ac1da061125e61e66dc51cc10e43fed61d
Quill Cronwall <quilljazz@gmail.com>
2024-11-20 17:30:26 +0900

    Refactor tests

--- hello_test.go
+++ hello_test.go
@@ -7,10 +7,20 @@ func TestHello(t *testing.T) {
 
 		got := Hello("teslazonda")
 		want := "Hello, teslazonda"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("empty sting defaults to 'world'", func(t *testing.T) {
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
 	})
 
+}
+
+func assertCorrectMessage(t testing.TB, got string, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
