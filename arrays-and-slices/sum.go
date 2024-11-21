83808152a8329956b17988868604208079d2bcc2
Quill Cronwall <quilljazz@gmail.com>
2024-11-21 21:21:11 +0900

    Complete arrays and slices lesson

--- arrays-and-slices/sum.go
+++ arrays-and-slices/sum.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func Sum(numbers []int) int { // numbers is a array of 5 elements. Array sizes are fixed.
 	sum := 0
 	for _, number := range numbers { // ranges returns two values index, value. Using the blank operator to ignore indexes.
@@ -15,10 +13,23 @@ func Sum(numbers []int) int { // numbers is a array of 5 elements. Array sizes a
 func SumAll(numbersToSum ...[]int) []int {
 	lengthOfNumbers := len(numbersToSum)
 	sums := make([]int, lengthOfNumbers)
-	fmt.Println("This is the sums", sums)
 
 	for i, numbers := range numbersToSum {
 		sums[i] = Sum(numbers)
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
 	}
 
 	return sums
--- arrays-and-slices/sum_test.go
+++ arrays-and-slices/sum_test.go
@@ -44,3 +44,27 @@ func TestSumAll(t *testing.T) {
 	}
 	// Note that reflect.DeepEqual is not type safe. It will compare any two variables regardless of their type, leading to unpredictable behavior. Only use for comparing slices.
 }
+
+// Sum of all items except the first one.
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("make the sums of tails of", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		checkSums(t, got, want)
+	})
+
+}
